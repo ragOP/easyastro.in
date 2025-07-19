b764886af45423ed00ccb7c62e049aaea30d02a1
VIRUS Gaming <30fpsgamingdude@gmail.com>
2025-07-19 20:55:32 +0000

    replace in header "https://ik.imagekit.io/5r36kvobl/WhatsApp%20Image%20J

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="py-4 px-4 sm:px-6 lg:px-8 bg-transparent z-10">
       <div className="container mx-auto flex items-center justify-center">
         <Image
-          src="https://ik.imagekit.io/5r36kvobl/May%2029%202025%20Screenshot%20Remove%20Background.png?updatedAt=1752955565962"
+          src="https://ik.imagekit.io/5r36kvobl/WhatsApp%20Image%20Jul%2020%202025.png"
           alt="Easy Astro Logo"
           width={200}
           height={50}
