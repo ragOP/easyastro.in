ec4f3df947a3e4c51972288c21eb988eacf0a7fc
VIRUS Gaming <30fpsgamingdude@gmail.com>
2025-07-19 20:57:43 +0000

    do a code check and strill unused parts of code that just slowing the we

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -55,14 +55,3 @@ export function ReadingIcon(props: SVGProps<SVGSVGElement>) {
         </svg>
     )
 }
-
-export function RevelationIcon(props: SVGProps<SVGSVGElement>) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-            <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2Z" />
-            <path d="M12 16v-4" />
-            <path d="M12 8h.01" />
-            <path d="M2.7 10.3A10 10 0 0 1 12 2a10 10 0 0 1 9.3 8.3" />
-        </svg>
-    )
-}
