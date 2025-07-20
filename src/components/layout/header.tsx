9310d34c8a49922d015e04ac4c02895a1c28b873
VIRUS Gaming <30fpsgamingdude@gmail.com>
2025-07-20 10:28:55 +0000

    change the header image ti this "https://ik.imagekit.io/5r36kvobl/ChatGP

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="py-4 px-4 sm:px-6 lg:px-8 bg-transparent z-10">
       <div className="container mx-auto flex items-center justify-center">
         <Image
-          src="https://ik.imagekit.io/5r36kvobl/Image%20Remove%20Background.png"
+          src="https://ik.imagekit.io/5r36kvobl/ChatGPT%20Image%20Jul%2020%202025.png"
           alt="Easy Astro Logo"
           width={200}
           height={50}
