40830f2e712c8cf61614bef1817d0e9b1adaaa03
VIRUS Gaming <30fpsgamingdude@gmail.com>
2025-07-20 10:57:08 +0000

    <!-- Meta Pixel Code --> <script>   !function(f,b,e,v,n,t,s)   {if(f.fbq

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,26 @@ export default function RootLayout({
             })(window, document, "clarity", "script", "shtoaztpqt");
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2413312665523975');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+              src="https://www.facebook.com/tr?id=2413312665523975&ev=PageView&noscript=1"/>
+        </noscript>
         {children}
         <Toaster />
       </body>
