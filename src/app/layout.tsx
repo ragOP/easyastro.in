91e1b9079b2de7d3b91edeb214d9fd9a77fde402
ragOP <najmiraghib@gmail.com>
2025-08-06 20:19:46 +0530

    ds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { Alegreya, Belleza } from 'next/font/google';
@@ -30,15 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${alegreya.variable} ${belleza.variable} !scroll-smooth`}>
       <head>
+        {/* Microsoft Clarity */}
         <Script id="clarity-analytics" strategy="afterInteractive">
           {`
             (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "shtoaztpqt");
           `}
         </Script>
+
+        {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -53,12 +56,36 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WRP6CQRV');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        {/* Meta Pixel NoScript */}
         <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-              src="https://www.facebook.com/tr?id=2413312665523975&ev=PageView&noscript=1"/>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2413312665523975&ev=PageView&noscript=1"
+          />
         </noscript>
+
+        {/* Google Tag Manager NoScript */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRP6CQRV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         {children}
         <Toaster />
       </body>
