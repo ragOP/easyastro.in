ccff96949dc404a462ea58f8d3fe3c6f70b08add
ragOP <najmiraghib@gmail.com>
2025-08-31 00:07:25 +0530

    dsds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,58 +30,26 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${alegreya.variable} ${belleza.variable} !scroll-smooth`}>
       <head>
-        {/* Microsoft Clarity */}
-        <Script id="clarity-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="afterInteractive">
           {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "shtoaztpqt");
-          `}
-        </Script>
-
-        {/* Meta Pixel 1 */}
-        <Script id="meta-pixel-1" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1330934167928475');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-
-        {/* Meta Pixel 2 */}
-        <Script id="meta-pixel-2" strategy="afterInteractive">
-          {`
-            fbq('init', '3960073624225686');
-            fbq('track', 'PageView');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WRP6CQRV');
           `}
         </Script>
       </head>
       <body className="font-body antialiased">
-        {/* Meta Pixel NoScript */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1330934167928475&ev=PageView&noscript=1"
-          />
-        </noscript>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=3960073624225686&ev=PageView&noscript=1"
-          />
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRP6CQRV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
         </noscript>
 
         {children}
