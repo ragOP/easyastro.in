f5cbcf8e26c27febf44ae9137db627705b04b518
ragOP <najmiraghib@gmail.com>
2026-01-08 18:42:16 +0530

    ds

---
+++ src/app/hindi-2/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="https://hindi.easyastra.in/"
+        title="EasyAstra Quiz"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        allow="camera; microphone; fullscreen"
+      />
+    </div>
+  );
+}
