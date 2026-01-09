267469c42b1eeb154d6e8098deb0e4eb03345b88
ragOP <najmiraghib@gmail.com>
2026-01-09 17:29:03 +0530

    dsds

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="https://destiny-canvas-maker.lovable.app/checkout"
+        title="EasyAstra Quiz"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        allow="camera; microphone; fullscreen"
+                sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
+
+
+
+      />
+    </div>
+  );
+}
